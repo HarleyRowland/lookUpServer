bf097aea39ef13018f540a80972eefa8ec7dfda5
Harley Rowland <harleyrowland17@gmail.com>
2016-01-31 22:54:55 +0000

    update sql sump

--- dump.sql
+++ dump.sql
@@ -1,12 +1,14 @@
-DROP TABLE IF EXISTS userLocation, userSettings, user;
+/*Backend bootstrap SQL*/
+DROP TABLE IF EXISTS userAuthentication, userLocation, userSettings, user;
 
+/*Create table statements*/
 CREATE TABLE user(
-    userID VARCHAR(25),
+    userID VARCHAR(15),
     PRIMARY KEY(userID)
 );
 
 CREATE TABLE userSettings(
-    userID      VARCHAR(25),
+    userID      VARCHAR(15),
     play        BOOL,
     FOREIGN KEY(userID)
         REFERENCES user(userID)
@@ -15,7 +17,7 @@ CREATE TABLE userSettings(
 );
 
 CREATE TABLE userLocation(
-    userID      VARCHAR(25),
+    userID      VARCHAR(15),
     longitude   DOUBLE(11, 8),
     latitude    DOUBLE(10, 8),
     timestamp   TIMESTAMP,
@@ -25,6 +27,24 @@ CREATE TABLE userLocation(
         ON UPDATE CASCADE
 );
 
+CREATE TABLE userAuthentication(
+    userID              VARCHAR(15),
+    password            VARCHAR(30),
+    authenticationCode  CHAR(5),
+    userAuthenticated   BOOL,
+        FOREIGN KEY(userID)
+        REFERENCES user(userID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE transactionHistory(
+    userID      VARCHAR(15),
+    messageSent VARCHAR(30),
+    timestamp   TIMESTAMP
+);
+
+/*Insert population statements*/
 INSERT INTO user VALUES('+447813646238');
 INSERT INTO user VALUES('+447535620820');
 INSERT INTO user VALUES('+447528441371');
@@ -43,3 +63,8 @@ INSERT INTO userLocation VALUES('447528441371', 000.00000000, 00.00000000, NOW()
 INSERT INTO userLocation VALUES('447427632795', 000.00000000, 00.00000000, NOW());
 INSERT INTO userLocation VALUES('447964129140', 000.00000000, 00.00000000, NOW());
 
+INSERT INTO userAuthentication VALUES('447813646238', 'password101', 'ce124', 1);
+INSERT INTO userAuthentication VALUES('447535620820', 'computer2000', 'cg546', 1);
+INSERT INTO userAuthentication VALUES('447528441371', 'keyboard3', 'ju878', 1);
+INSERT INTO userAuthentication VALUES('447427632795', 'more_complicated!@09', 'g8h92', 1);
+INSERT INTO userAuthentication VALUES('447964129140', 'MyDogsNameIs fluffy', '09802', 0);
